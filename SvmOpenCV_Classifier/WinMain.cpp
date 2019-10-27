76378bd643e8f1c6c4ff4c201298cb07438b466a
vohungvi <vohungvi27@gmail.com>
2019-10-27 16:23:38 +0700

    - compile success

---
+++ SvmOpenCV_Classifier/WinMain.cpp
@@ -0,0 +1,93 @@
+// TGMTtemplate.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <tchar.h>
+#include <map>
+
+#include "TGMTfile.h"
+#include "TGMTdebugger.h"
+#include "TGMTsvm.h"
+#include "TGMTutil.h"
+#include "TGMTConfig.h"
+#include <time.h>
+
+#define INI_SECTION "SVM_classifier"
+
+void PrintHelp()
+{
+	PrintMessage("This program auto classify and move file to each folder");
+	std::cout << "Using with syntax: \n";
+	debug_out(3, "SvmOpenCV_Classifier.exe -file <svm_file> -in <directory> -out <directory> -w <width> -h <height>\n");
+	std::cout << "with <svm_file> is SVM trained file\n";
+	std::cout << "and <directory> is directory contain images to predict\n";
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CreateEmptyFolder(std::string dir)
+{
+	TGMTfile::CreateDir(dir);
+	for (int i = 0; i < 10; i++)
+	{
+		TGMTfile::CreateDir(TGMTutil::FormatString("%s%d", dir.c_str(), i));
+	}
+
+	for (char i = 'A'; i <= 'Z'; i++)
+	{
+		TGMTfile::CreateDir(TGMTutil::FormatString("%s%c", dir.c_str(), i));
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	GetTGMTConfig()->LoadSettingFromFile();
+
+	std::string svmFile = GetTGMTConfig()->ReadValueString(INI_SECTION, "input_file");
+	std::string inDir = GetTGMTConfig()->ReadValueString(INI_SECTION, "input_dir");
+	std::string outDir = GetTGMTConfig()->ReadValueString(INI_SECTION, "output_dir");
+	outDir = TGMTfile::CorrectPath(outDir);
+	CreateEmptyFolder(outDir);
+
+
+	clock_t startTime = clock();
+
+	PrintMessage("Loading data...");
+	GetTGMTsvm()->LoadData(svmFile);
+
+	PrintMessage("Loading image...");
+	std::vector<std::string> files = TGMTfile::GetImageFilesInDir(inDir);
+	PrintMessage("Loaded %d images", files.size());
+
+	std::map<int, int> maps;
+
+	for (int i = 0; i < files.size(); i++)
+	{
+		SET_CONSOLE_TITLE("%d / %d", i + 1, files.size());
+
+		int result =  GetTGMTsvm()->Predict(files[i]);
+		maps[result]++;
+
+		std::string fileName = TGMTfile::GetFileName(files[i]);
+		PrintMessage("%s: %c", fileName.c_str(), (char)result);
+		std::string targetFile = TGMTutil::FormatString("%s%c\\%s", outDir.c_str(), result, fileName.c_str());
+		
+		TGMTfile::MoveFileAsync(files[i], targetFile);
+	}
+
+	//Print Result
+	PrintMessageBlue("Sum up:");
+	std::map<int, int>::iterator iter = maps.begin();
+	while (iter != maps.end())
+	{
+		PrintMessage("Class %c: %d", (char)iter->first, iter->second);
+		iter++;
+	}
+
+	PrintSuccess("Classify complete");
+	int elapsedTime = clock() - startTime;
+	getchar();
+	return 0;
+}
---
+++ SvmOpenCV_Training/WinMain.cpp
@@ -0,0 +1,40 @@
+// TGMTtemplate.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <tchar.h>
+#include "TGMTfile.h"
+#include "TGMTdebugger.h"
+#include "TGMTsvm.h"
+#include "TGMTutil.h"
+#include "TGMTConfig.h"
+
+#define INI_SECTION "SVM_training"
+
+void PrintHelp()
+{
+	std::cout << "Using with syntax: \n";
+	debug_out(3, "SvmOpenCV_Training.exe -in <directory> -out <file> -w <width> -h <height>\n");
+		
+	std::cout << "-in is directory contain sub directories, with each sub directory contain images same class, name of each sub directory only 1 character\n"
+		<< "-out is file name you choose\n";
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void _tmain(int argc, _TCHAR* argv[])
+{
+	GetTGMTConfig()->LoadSettingFromFile();
+
+	std::string dir = GetTGMTConfig()->ReadValueString(INI_SECTION, "input_dir");
+	std::string fileOuput = GetTGMTConfig()->ReadValueString(INI_SECTION, "output_file");
+
+	if (!TGMTfile::DirExist(dir))
+	{
+		PrintError("Directory \"%s\" does not exist", dir.c_str());
+		return;
+	}
+	
+	GetTGMTsvm()->TrainData(dir);
+	GetTGMTsvm()->SaveData(fileOuput);
+}
